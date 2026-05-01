b6fff8a8f872f66027d7aa5ffde2159e77576c1b
PIXELL07 <arunksn007@gmail.com>
2026-05-02 00:04:57 +0530

    feat: update write in ws

--- ws/write_pump.go
+++ ws/write_pump.go
@@ -1,4 +1,35 @@
 package ws
 
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/pixell07/equalink/hub"
+)
+
 // WritePump drains the client Send channel → pushes to WebSocket.
 // Sends pings to detect dead connections.
+func WritePump(client *hub.Client, conn *websocket.Conn) {
+	ticker := time.NewTicker(PingPeriod)
+	defer func() {
+		ticker.Stop()
+		conn.Close()
+	}()
+
+	for {
+		select {
+		case msg, ok := <-client.Send:
+			conn.SetWriteDeadline(time.Now().Add(WriteWait))
+			if !ok {
+				conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			conn.WriteMessage(websocket.BinaryMessage, msg)
+		case <-ticker.C:
+			conn.SetWriteDeadline(time.Now().Add(WriteWait))
+			if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
